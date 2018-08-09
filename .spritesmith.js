c8e00067473cb644e8cb5a050b59c49e6bb5fdec
feelic <feelics@gmail.com>
2018-08-09 19:59:48 +0200

    feat(sprite): add sprite script system

---
+++ .spritesmith.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var util = require('util');
+
+module.exports = {
+  src: './src/assets/thumbs/*.{png,gif,jpg}',
+  destImage: './src/assets/card-sprite.png',
+  destCSS: './src/assets/card-sprite.css'
+};
