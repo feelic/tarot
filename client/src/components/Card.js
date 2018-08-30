85323bd41db31e75fcdfdc712db6a0961ac7adcb
feelic <feelics@gmail.com>
2018-08-30 21:50:22 +0200

    feat(client): disabled card styles

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -3,10 +3,22 @@ import './Card.css';
 import '../assets/card-sprite.css';
 
 export default props => {
-  const {onCardClick, card} = props;
+  const {onCardClick, card, disabled, tooltip} = props;
   const cardClass = (card && `icon-${card}`) || 'card-turned';
+  const hoverableClass = card && ! disabled && 'card-hoverable';
+  const tooltipAttribute = {};
+
+  if (tooltip) {
+    tooltipAttribute.title = tooltip;
+  }
 
   return (
-    <div onClick={() => onCardClick && onCardClick(card)} className={`card ${cardClass}`} />
+    <div
+      onClick={() => onCardClick && ! disabled && onCardClick(card)}
+      className={`card ${cardClass} ${hoverableClass}`}
+      {...tooltipAttribute}
+    >
+      {disabled && <div className="card-disable-overlay" />}
+    </div>
   );
 };
