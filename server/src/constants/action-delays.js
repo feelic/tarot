cba037ebf0bec87d2a6a7fc31d82d3e86b402141
feelic <feelics@gmail.com>
2020-04-26 16:54:30 +0200

    fix(server): update action delays

--- server/src/constants/action-delays.js
+++ server/src/constants/action-delays.js
@@ -1,8 +1,8 @@
-export const BOT_PLAY_CARD_DELAY = 20;
-export const BOT_PLACE_BID_DELAY = 20;
-export const BOT_MAKE_CHIEN_DELAY = 100;
+export const BOT_PLAY_CARD_DELAY = 300;
+export const BOT_PLACE_BID_DELAY = 400;
+export const BOT_MAKE_CHIEN_DELAY = 3000;
 
-export const SERVER_AWARD_BID_DELAY = 100;
-export const SERVER_BIDDING_FAILED_DELAY = 100;
-export const SERVER_AWARD_TRICK_DELAY = 100;
-export const SERVER_AWARD_ROUND_DELAY = 100;
+export const SERVER_AWARD_BID_DELAY = 2000;
+export const SERVER_BIDDING_FAILED_DELAY = 2000;
+export const SERVER_AWARD_TRICK_DELAY = 1000;
+export const SERVER_AWARD_ROUND_DELAY = 1000;
